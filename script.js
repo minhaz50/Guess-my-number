60a2582aa7024a53a5e5e65e92fbeafca74bc328
minhaz2597 <md.minhazuddin2597@gmail.com>
2022-02-09 21:58:46 +0600

    line added in js file

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉Corrrect Number';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+document.querySelector('.guess').value = 15;
+console.log(document.querySelector('.guess').value);
